c64ceef584295451edc53d868bc4b9ab66c4ca9f
rkpanchal2202-sudo <rkpanchal2202@gmail.com>
2026-08-12 10:55:49 +0530

    Add files via upload

---
+++ 26BCPT363 5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    /*convert hours into minutes */
+    int a,b;
+    printf("enter 1 nos.");
+    scanf("%d",&a);
+    b=a*60;
+    printf("%d * 60= %d\n", a,b);
+    return 0;
+}

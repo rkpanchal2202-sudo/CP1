0ce7ada82722e55c56fbce41807a0e602bf3edb1
rkpanchal2202-sudo <rkpanchal2202@gmail.com>
2026-08-12 10:49:36 +0530

    Add files via upload

---
+++ 26BCPT363 4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    /*program to divide 2 nos. */
+    float a,b,c;
+    printf("enter 2 nos.");
+    scanf("%f%f",&a,&b);
+    c=a/b;
+    printf("%f / %f = %f\n", a,b,c);
+    return 0;
+}

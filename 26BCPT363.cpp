2c3cdfef2f12736ed32e7a4117a37596dfa5fced
rkpanchal2202-sudo <rkpanchal2202@gmail.com>
2026-08-12 09:56:42 +0530

    Add files via upload

---
+++ 26BCPT363.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    /*program to add 2 nos. */
+    int a,b,c;
+    printf("enter 2 nos.");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("%d + %d = %d\n", a,b,c);
+    return 0;
+}
